de7d978e938afc9838f9a41745d53b8bfb718336
Peter Wagner <peter.wagner1986@gmail.com>
2021-11-26 13:04:43 +0100

    connected backend to frontend

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.post('/post', (req, res) => {
+    console.log('Connected to React');
+    // res.redirect('/');
 });
 
 app.listen(port, () => {
