f6c9b6261d61866a3a54a1befa494a17a3e9952f
Peter Wagner <peter.wagner1986@gmail.com>
2021-12-05 14:02:16 +0100

    finally managed to reseve message fron frontend via click-event

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const io = require('socket.io')(3001, {
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const PORT = process.env.PORT;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:3000' || PORT,
         methods: ['GET', 'POST'],
     },
 });
 
-io.on('connection', socket => {
-    console.log('backend connected');
-})
+app.get('/', (req, res) => res.send('Willkommen auf dem Server von Youtube and Chill'));
 
-io.on('click-event', socket => console.log('Play-Button clicked'));
+httpServer.listen(3001)
+
+io.on('connection', socket => {
+    console.log(socket.id, socket.connected);
+    // socket.emit('hello', 'world');
+    socket.on('click-event', arg => console.log(arg));
+})
